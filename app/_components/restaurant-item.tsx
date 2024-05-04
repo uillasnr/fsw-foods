3e1e185520d82bbee703d0286988a82e4907daa1
uillasnr <wiliasreis@hotmail.com>
2024-05-04 15:56:56 -0300

    feat: add restaurant list component

---
+++ app/_components/restaurant-item.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Restaurant } from "@prisma/client";
+import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
+import Image from "next/image";
+import { formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+interface RestaurantItemProps {
+  restaurant: Restaurant;
+  /*  className?: string;
+  userFavoriteRestaurants: UserFavoriteRestaurant[]; */
+}
+
+const RestaurantItem = ({
+  restaurant,
+  /*  className,
+  userFavoriteRestaurants, */
+}: RestaurantItemProps) => {
+  /*  const { data } = useSession();
+  const isFavorite = userFavoriteRestaurants.some(
+    (fav) => fav.restaurantId === restaurant.id,
+  ); */
+
+  /*  const handleFavoriteClick = async () => {
+    if (!data?.user.id) return;
+    try {
+      await toggleFavoriteRestaurant(data?.user.id, restaurant.id);
+      toast.success(
+        isFavorite
+          ? "Restaurante removido dos favoritos."
+          : "Restaurante favoritado.",
+      );
+    } catch (error) {
+      toast.error("Erro ao favoritar restaurante.");
+    }
+  }; */
+
+  return (
+    <div className="min-w-[266px] max-w-[266px]">
+      <div className="w-full space-y-3">
+        {/* IMAGEM */}
+        <div className="relative h-[136px] w-full">
+          <Link href={`/restaurants/${restaurant.id}`}>
+            <Image
+              src={restaurant.imageUrl}
+              fill
+              className="rounded-lg object-cover"
+              alt={restaurant.name}
+            />
+          </Link>
+
+          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary bg-white px-2 py-[2px]">
+            <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+            <span className="text-xs font-semibold">5.0</span>
+          </div>
+
+          <Button
+            size="icon"
+            className={`"bg-primary absolute right-2 top-2 h-7 w-7 rounded-full  bg-gray-700 hover:bg-gray-700`}
+          >
+            <HeartIcon size={16} className="fill-white" />
+          </Button>
+        </div>
+        {/* TEXTO */}
+        <div>
+          <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+          {/* INFORMAÇÕES DA ENTREGA */}
+          <div className="flex gap-3">
+            {/* CUSTO DE ENTREGA */}
+            <div className="flex items-center gap-1">
+              <BikeIcon className="text-primary" size={14} />
+              <span className="text-xs text-muted-foreground">
+                {Number(restaurant.deliveryFee) === 0
+                  ? "Entrega grátis"
+                  : formatCurrency(Number(restaurant.deliveryFee))}
+              </span>
+            </div>
+            {/* TEMPO DE ENTREGA */}
+            <div className="flex items-center gap-1">
+              <TimerIcon className="text-primary" size={14} />
+              <span className="text-xs text-muted-foreground">
+                {restaurant.deliveryTimeMinutes} min
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantItem;
