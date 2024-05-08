bd5338663c0e5fc81acbd6d6397eef1ff76d9692
uillasnr <wiliasreis@hotmail.com>
2024-05-07 22:54:17 -0300

    styles: button search

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -32,7 +32,7 @@ const Search = () => {
         onChange={handleChange}
         value={search}
       />
-      <Button size="icon" type="submit">
+      <Button size="icon" type="submit" className="md:bg-[#FFB100]">
         <SearchIcon size={20} />
       </Button>
     </form>
