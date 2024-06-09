7431718d36c4c400b5188a176a0621c856816fef
uillasnr <wiliasreis@hotmail.com>
2024-06-09 19:27:00 -0300

    feat: Next Auth function of being able to log in with email and password

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -3,20 +3,67 @@ import { AuthOptions } from "next-auth";
 import { db } from "./prisma";
 import GoogleProvider from "next-auth/providers/google";
 import { Adapter } from "next-auth/adapters";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await db.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const isValidPassword = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isValidPassword) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
   ],
+
   callbacks: {
-    async session({ session, user }) {
-      session.user = { ...session.user, id: user.id };
-      return session;
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+      }
+
+      return token;
     },
   },
+  pages: {
+    signIn: "/login",
+  },
+
   secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
 };
